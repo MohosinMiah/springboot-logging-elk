b7d58617dcd27b22264adabd334052ef19e5ca81
mohosinmiah <hamza1610330816@gmail.com>
2023-08-12 00:46:00 +0600

    ELK Version One -> Update Helloworld Controller return message

--- src/main/java/com/elk/elk/controller/HelloWorldController.java
+++ src/main/java/com/elk/elk/controller/HelloWorldController.java
@@ -30,9 +30,31 @@ public class HelloWorldController {
         // Prepare log response
         String helloWorldLog = prepareHHelloWorldLogRespose();
         log.info(helloWorldLog);
-        // log.error(helloWorldLog);
-        // log.warn(helloWorldLog);
-        return "This is Demo Home API";
+        return "INFO -> Home Page";
+    }
+
+
+
+    @GetMapping("/logs")
+    public String LogsPage() throws JsonProcessingException{
+        String helloWorldLog = prepareHHelloWorldLogRespose();
+        log.warn(helloWorldLog);
+        return "LOGS";
+    }
+
+    @GetMapping("/warn")
+    public String WarnPage() throws JsonProcessingException{
+        String helloWorldLog = prepareHHelloWorldLogRespose();
+        log.warn(helloWorldLog);
+        return "WARN";
+    }
+
+
+    @GetMapping("/er")
+    public String ErrorPage() throws JsonProcessingException{
+        String  helloWordLog = prepareHHelloWorldLogRespose();
+        log.error(helloWordLog);
+        return "ERROR";
     }
 
     private String prepareHHelloWorldLogRespose() throws JsonProcessingException {
@@ -42,27 +64,5 @@ public class HelloWorldController {
         helloWorldLog.setMessage(" This is INFO from Home Page");
 
         return objectMapper.writeValueAsString(helloWorldLog);
-    }
-
-    @GetMapping("/logs")
-    public String LogsPage(){
-        LocalDateTime localDateTime = LocalDateTime.now();
-        log.info("This Logs page " + localDateTime);
-        return "Welcome to logs page";
-    }
-
-    @GetMapping("/warn")
-    public String WarnPage(){
-        LocalDateTime localDateTime = LocalDateTime.now();
-        log.warn("This warn page " + localDateTime);
-        return "Welcome to warn page";
-    }
-
-
-    @GetMapping("/er")
-    public String ErrorPage(){
-        LocalDateTime localDateTime = LocalDateTime.now();
-        log.error("This error page " + localDateTime);
-        return "Welcome to error page";
     }
 }
