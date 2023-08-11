49e4c1d3f0b178457359748a133ec4d06c9a0016
mohosinmiah <hamza1610330816@gmail.com>
2023-08-11 22:38:09 +0600

    Initial Setup and ELK docker compose file added

---
+++ src/test/java/com/elk/elk/ElkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elk.elk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
